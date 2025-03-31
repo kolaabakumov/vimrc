d69e835fd97ac26a56587e2d6acf48f99e3a5679
kolaabakumov <122364112+kolaabakumov@users.noreply.github.com>
2025-03-31 21:18:15 +0300

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # URL к вашему .vimrc в Gist
-VIMRC_URL="https://gist.githubusercontent.com/kolaabakumov/vimrc/main/.vimrc"
+VIMRC_URL="https://raw.githubusercontent.com/kolaabakumov/vimrc/main/.vimrc"
 
 # Скачиваем .vimrc
 curl -fsSL "$VIMRC_URL" -o ~/.vimrc
