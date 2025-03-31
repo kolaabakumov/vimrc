a2801464e6ff5d9c7d4da44cf9c15c51396d3063
kolaabakumov <122364112+kolaabakumov@users.noreply.github.com>
2025-03-31 21:27:48 +0300

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -5,19 +5,3 @@ VIMRC_URL="https://raw.githubusercontent.com/kolaabakumov/vimrc/main/.vimrc"
 
 # Скачиваем .vimrc
 curl -fsSL "$VIMRC_URL" -o ~/.vimrc
-
-# Устанавливаем Vim, если не установлен
-if ! command -v vim &> /dev/null; then
-    echo "Vim не установлен. Устанавливаю..."
-    sudo apt update && sudo apt install -y vim
-fi
-
-# Установка vim-plug, если ещё не установлен
-if [ ! -f ~/.vim/autoload/plug.vim ]; then
-    echo "Устанавливаю vim-plug..."
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-# Устанавливаем плагины, если они есть
-vim +PlugInstall +qall
